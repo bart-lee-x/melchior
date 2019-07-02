f56a7ab799b82f2d8041ae410042d2df002a92e0
bart <bart.lee@kakaocorp.com>
2019-07-03 00:12:34 +0900

    null 인 component는 json에서 포함하지 않는 테스트 추가

--- test/ToJsonTest.py
+++ test/ToJsonTest.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 
 from melchior.model.template.output.component import SimpleText, Component
@@ -7,11 +8,14 @@ class TestToJsonSimple(unittest.TestCase):
 
     def test_simple(self):
         simple_text = SimpleText('hi')
-        print(simple_text.to_json())
         self.assertEqual('hi', simple_text.text)
 
     def test_component_with_simple_text_only(self):
         simple_text = SimpleText('hell')
         component_with_simple_text = Component(simpleText=simple_text)
-        print(component_with_simple_text.to_json())
         self.assertEqual('hell', component_with_simple_text.simpleText.text)
+
+        converted_component = json.loads(component_with_simple_text.to_json())
+        self.assertEqual('hell', converted_component['simpleText']['text'])
+        with self.assertRaises(KeyError):
+            converted_component['simpleImage']
